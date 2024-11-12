716a91b03a1d2b5f47c5ba2242f8887e56b5713d
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-11-12 16:13:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,14 @@ import Login from "./components/Login/Login/Login";
 import ChatApp from "./components/Chat-Sys/ChatApp";
 import SetPasswordPage from "./components/Login/SetPasswordPage";
 import VerifyPage from "./components/Login/VerifyCode";
-import SecurityPage from './SecurityPage.jsx';
+
 
 function App() {
-  const user = localStorage.getItem("token");
-  const securityCodeEntered = localStorage.getItem('securityCodeEntered');
 
   return (
     <BrowserRouter>
       <Routes>
         {user && <Route path="/" exact element={<ChatApp />} />}
-        {!securityCodeEntered && <Route path="/" element={<SecurityPage />} />}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgotpassword" element={<Forgot />} />
